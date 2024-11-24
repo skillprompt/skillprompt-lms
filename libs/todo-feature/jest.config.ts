b3f302722a8f887e014f95a87a270e17b1ae424e
baijanaththaru-skillprompt <baijanaththaru@skillprompt.com>
2024-11-24 11:48:18 +0545

    generate sample node library

---
+++ libs/todo-feature/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'todo-feature',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/todo-feature',
+};
---
+++ libs/todo-feature/src/lib/todo-feature.spec.ts
@@ -0,0 +1,7 @@
+import { todoFeature } from './todo-feature';
+
+describe('todoFeature', () => {
+  it('should work', () => {
+    expect(todoFeature()).toEqual('todo-feature');
+  });
+});
