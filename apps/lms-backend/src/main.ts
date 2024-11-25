7b23d5dc5b07d1cc75e67ec1d374da554353c3bb
Shahil-Ahamad-dev <shahilahamadskillprompt@gmail.com>
2024-11-25 14:06:37 +0545

    Created Backend_Setup File Folder Structure is in Queue

--- apps/lms-backend/src/main.ts
+++ apps/lms-backend/src/main.ts
@@ -35,7 +35,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
 // ------------------------- CORS Setup -------------------------
 app.use(
   cors({
-    origin: ['http://your-frontend-url.com'], // replace with your actual frontend URL
+    origin: [`${env.FRONTEND_URI}`], // replace with your actual frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Support cookies
   })
--- apps/lms-backend/src/utils/config.ts
+++ apps/lms-backend/src/utils/config.ts
@@ -8,6 +8,7 @@ export const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   HOST: process.env.HOST ?? 'localhost',
   PORT: Number(process.env.PORT) || 4000,
+  FRONTEND_URI : 'frontend_uri',
   //   DataBase_URI: process.env.DataBase_URI || "",
   JWT_SECRET: process.env.JWT_SECRET || '',
 } as const;
